c11deef1e366fa6fcfa93e81991024002472c453
Dizerro <zakzaksukhar@gmail.com>
2024-09-16 20:11:22 +0300

    Lab 1

---
+++ lab1/Music.py
@@ -0,0 +1,21 @@
+music = "thekomakoma"
+print ("Виконавець:" , music)
+
+is_adult = True 
+is_teen = False 
+_Task_1 = is_adult or is_teen
+print (_Task_1)
+_Task_2 = is_adult and is_teen
+print (_Task_2)
+_Task_3 = not is_teen , not is_adult
+print (_Task_3)
+
+is_sunny = True 
+is_cloudy = True
+_Task_4 = not is_sunny , not is_cloudy
+print (_Task_4)
+
+x=1.413
+y=0.057
+_equation = 77*x**12+0.3*y**9+x**6/(x**2+y**2)+y**4+y**3-1.111
+print (_equation)
