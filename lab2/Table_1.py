b1209b26ea029bcd7caecab8c60534a3cdbd2d6f
Dizerro <zakzaksukhar@gmail.com>
2024-09-23 18:45:26 +0300

    lab 2 table 1.5

---
+++ lab2/Table_1.py
@@ -0,0 +1,19 @@
+from math import log , log10 , cos , tan
+print("Введіть значення X в проміжку [2;5]:")
+x = float(input())
+a = 2
+b = 5
+h = 0.2
+while a <= x <= b:
+    if x < 3:
+        y=log10(abs(log(x)+1/(cos(x))))
+        print("x=",x, "y=", y)
+    elif 3<=x<4:
+        y = 1/tan(x+log(x))
+        print("x=",x, "y=", y)
+    elif x > 4:
+        y = 1/(16-x**2)
+        print("x=",x, "y=", y)
+    x += h
+else:
+    print("виввели число не з проміжку")
