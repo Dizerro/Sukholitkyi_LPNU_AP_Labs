aa357b6957b69d794a7dda5086d20a627bfed384
Dizerro <zakzaksukhar@gmail.com>
2024-09-23 19:17:09 +0300

    readeble

--- lab2/Table_1.py
+++ lab2/Table_1.py
@@ -1,16 +1,21 @@
 from math import log , log10 , cos , tan
-x = 2
+
 a = 2
 b = 5
 h = 0.2
+
+x = 2
 while a <= x <= b:
     if x < 3:
-        y=log10(abs(log(x)+1/(cos(x))))
-        print("x=",x, "y=", y)
+        y=log10(abs(log(x) + 1/(cos(x))))
+        print(f"x=",{x}, "y=", {y})
+
     elif 3<=x<4:
-        y = 1/tan(x+log(x))
-        print("x=",x, "y=", y)
+        y = 1/tan(x + log(x))
+        print(f"x=",{x}, "y=", {y})
+    
     elif x > 4:
-        y = 1/(16-x**2)
-        print("x=",x, "y=", y)
+        y = 1/(16 - x**2)
+        print(f"x=",{x}, "y=", {y})
+
     x += h
---
+++ lab2/Table_2.py
@@ -0,0 +1,11 @@
+from math import factorial
+
+h = 0.05
+a = 0
+b = 0.5
+d = 0.001
+
+x = 0
+
+while a<=x<=b:
+    m = 20
