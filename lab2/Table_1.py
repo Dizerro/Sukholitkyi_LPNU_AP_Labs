af58e9f5152ea647421953c9dd77b92a7706d038
Dizerro <zakzaksukhar@gmail.com>
2024-09-24 12:22:48 +0300

    better print

--- lab2/Table_1.py
+++ lab2/Table_1.py
@@ -8,14 +8,14 @@ x = a
 while a <= x <= b:
     if x < 3:
         y=log10(abs(log(x) + 1/(cos(x))))
-        print(f"x=",{x}, "y=", {y})
+        print(f"x=,{x:.2f}, y=, {y}")
 
     elif 3<=x<4:
         y = 1/tan(x + log(x))
-        print(f"x=",{x}, "y=", {y})
+        print(f"x=,{x:.2f}, y=, {y}")
     
     elif x > 4:
         y = 1/(16 - x**2)
-        print(f"x=",{x}, "y=", {y})
+        print(f"x=,{x:.2f}, y=, {y}")
 
     x += h
